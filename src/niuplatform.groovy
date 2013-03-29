ef6a45c7ced76e57c979996eee32a47f986f9d41
jiangjianbo <jiangjianbo@gmail.com>
2013-03-29 17:15:52 +0800

    add code

--- src/niuplatform.groovy
+++ src/niuplatform.groovy
@@ -9,20 +9,53 @@
   */
 
 
-
-ant = new AntBuilder();
-
-
-static void main(args){
-    if( args.length > 1 && args[0] == "-h" ){
-        println '''usageg: NiuPlatform [-h|config]
+// process command line
+if( args.length == 0 || args[0] == "-h" ){
+    println '''usageg: NiuPlatform [-h|config]
     -h      show this message
     config  config file or path, default is '.'
-                '''
-     return;
-    }
+            '''
+    return;
+}
+
+// read config
+var platform_base = "";
+
+// initialize
+ant = new AntBuilder();
+
+// create structure
+ant.mkdir(dir:"${platform_base}/program");
+ant.mkdir(dir:"${platform_base}/data");
+ant.mkdir(dir:"${platform_base}/tools/packages");
+
+install_joomla();
+install_drupal();
+
+function install_joomla(){
+    require_apache();
+    require_php();
+    require_mysql();
+
+    download_joomla();
+    extract_joomla();
+    config_joomla();
+}
+
+
+
+function download_apache(){
 
 }
+
+function install_apache(){
+
+}
+
+
+
+
+
 
 
 
